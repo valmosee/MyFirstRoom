ab0976dc08ab4177a011fdf1522a43433e6bb836
Joice <c14230225@john.petra.ac.id>
2025-12-02 17:09:23 +0700

    No. 20 - 26

--- app/src/main/java/codelab/c14230225/myfirstroom/MainActivity.kt
+++ app/src/main/java/codelab/c14230225/myfirstroom/MainActivity.kt
@@ -1,12 +1,23 @@
 package codelab.c14230225.myfirstroom
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.ActionMode
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import codelab.c14230225.myfirstroom.database.NoteRoomDatabase
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var DB : NoteRoomDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +26,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        DB = NoteRoomDatabase.getDatabase(this)
+        val _fabAdd = findViewById<ImageButton>(R.id.fabAdd)
+
+        _fabAdd.setOnClickListener {
+            startActivity(Intent(this, TambahData::class.java))
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        CoroutineScope(Dispatchers.Main).async {
+            val note = DB.funnoteDao().selectAll()
+            Log.d("data ROOM", note.toString())
         }
     }
 }
