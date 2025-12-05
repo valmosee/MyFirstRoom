80a53190703f8d7cf336e4d13bcccd82334cdc9e
Joice <c14230225@john.petra.ac.id>
2025-12-05 21:27:23 +0700

    No. 37 - 47

--- app/src/main/java/codelab/c14230225/myfirstroom/MainActivity.kt
+++ app/src/main/java/codelab/c14230225/myfirstroom/MainActivity.kt
@@ -9,14 +9,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import codelab.c14230225.myfirstroom.database.Note
 import codelab.c14230225.myfirstroom.database.NoteRoomDatabase
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var DB : NoteRoomDatabase
+    private lateinit var adapterN : adapterNote
+    private var arNote : MutableList<Note> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,11 +35,32 @@ class MainActivity : AppCompatActivity() {
         }
         DB = NoteRoomDatabase.getDatabase(this)
         val _fabAdd = findViewById<ImageButton>(R.id.fabAdd)
+        val _rvNotes : RecyclerView = findViewById<RecyclerView>(R.id.rvNotes)
 
         _fabAdd.setOnClickListener {
             startActivity(Intent(this, TambahData::class.java))
         }
 
+        adapterN = adapterNote(arNote)
+
+        _rvNotes.layoutManager = LinearLayoutManager(this)
+        _rvNotes.adapter = adapterN
+
+        adapterN.setOnItemClickCallback(
+            object : adapterNote.OnItemClickCallback{
+                override fun delData(dtnote : Note){
+                    CoroutineScope(Dispatchers.IO).async {
+                        DB.funnoteDao().delete(dtnote)
+                        val note = DB.funnoteDao().selectAll()
+                        Log.d("data ROOM2", note.toString())
+
+                        withContext(Dispatchers.Main){
+                            adapterN.isiData(note)
+                        }
+                    }
+                }
+            }
+        )
     }
 
     override fun onStart() {
@@ -41,6 +68,8 @@ class MainActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.Main).async {
             val note = DB.funnoteDao().selectAll()
             Log.d("data ROOM", note.toString())
+
+            adapterN.isiData(note)
         }
     }
 }
